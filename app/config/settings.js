35037e983a09ad7c13c85219bf9cc7a0886e3d9e
fmigch <128748261+fmigch@users.noreply.github.com>
2024-03-11 23:31:14 +0100

    Added api support for pickuplocations

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,1 +1,2 @@
-export const apiUrl = 'http://localhost:3000/paazl/api/shippingoptions.php'
+export const shippingOptionsUrl = 'http://localhost:3000/paazl/api/shippingoptions.php'
+export const pickupLocationsUrl = 'http://localhost:3000/paazl/api/pickuplocations.php'
--- app/public/script.js
+++ app/public/script.js
@@ -1,52 +1,59 @@
 import Alpine from '.././node_modules/alpinejs/dist/module.esm.js'
-import { apiUrl } from '.././config/settings.js'
+import { pickupLocationsUrl, shippingOptionsUrl } from '.././config/settings.js'
 import paazlService from '.././src/services/paazlService.js'
 import { titles, words, deliveryMethods, defaultDeliveryOptions } from '.././locale/nl.js'
 
-let deliveryDays,deliveryOptions,collectOptions  = []
-
+let deliveryDays, deliveryOptions, collectOptions = []
 
 Alpine.data('app', () => ({
-    titles: titles,
-    deliveryMethods: deliveryMethods,
-    deliveryDays: deliveryDays,
-    deliveryOptions: deliveryOptions,
-    collectOptions: collectOptions,
-    selectedDeliveryMethod: deliveryMethods[0].type,
-    selectedDeliveryDay: '',
-    selectedDeliveryOption: '',
-    selectedCollectOption: '',
+	titles: titles,
+	deliveryMethods: deliveryMethods,
+	deliveryDays: deliveryDays,
+	deliveryOptions: deliveryOptions,
+	collectOptions: collectOptions,
+	selectedDeliveryMethod: deliveryMethods[0].type,
+	selectedDeliveryDay: '',
+	selectedDeliveryOption: '',
+	selectedCollectOption: '',
 
-    async getData() {
-        try {
-            const response = await paazlService(apiUrl)
-            this.deliveryDays = response
-            this.deliveryOptions = response[0].options
-            this.selectedDeliveryOption = response[0].options[0].identifier
-        } catch {
-            console.log('Could not connect to Paazl')
+	async getShippingOptions() {
+		try {
+			const response = await paazlService(shippingOptionsUrl)
+			this.deliveryDays = response
+			this.deliveryOptions = response[0].options
+			this.selectedDeliveryOption = response[0].options[0].identifier
+		} catch {
+			console.log('Could not connect to Paazl')
 
-            let tomorrow = new Date()
-            tomorrow.setUTCDate(tomorrow.getUTCDate() + 1)
+			let tomorrow = new Date()
+			tomorrow.setUTCDate(tomorrow.getUTCDate() + 1)
 
-            this.deliveryDays = [
-                {
-                    'label': words.tomorrow[0].toUpperCase() + words.tomorrow.slice(1).toLowerCase(),
-                    'date': tomorrow.toISOString().substring(0, 10)
-                }
-            ]
+			this.deliveryDays = [
+				{
+					'label': words.tomorrow[0].toUpperCase() + words.tomorrow.slice(1).toLowerCase(),
+					'date': tomorrow.toISOString().substring(0, 10)
+				}
+			]
 
-            this.deliveryOptions = defaultDeliveryOptions,
-            this.selectedDeliveryDay = this.deliveryDays[0].date,
-            this.selectedDeliveryOption = defaultDeliveryOptions[0].identifier
-        }
-    },
+			this.deliveryOptions = defaultDeliveryOptions,
+				this.selectedDeliveryDay = this.deliveryDays[0].date,
+				this.selectedDeliveryOption = defaultDeliveryOptions[0].identifier
+		}
+	},
 
-    updateDeliveryOptions(event) {
-        const i = this.deliveryDays.findIndex(item => item.date === event.target.value)
-        this.deliveryOptions = this.deliveryDays[i].options
-        this.selectedDeliveryOption = this.deliveryDays[i].options[0].identifier
-    }
+	async getPickupLocations() {
+		if(this.collectOptions.length == 0) {
+			const response = await paazlService(pickupLocationsUrl)
+			this.collectOptions = response
+			console.log(response)
+		}
+	},
+
+	updateDeliveryOptions(event) {
+		const i = this.deliveryDays.findIndex(item => item.date === event.target.value)
+		this.deliveryOptions = this.deliveryDays[i].options
+		this.selectedDeliveryOption = this.deliveryDays[i].options[0].identifier
+	}
 }))
 
 Alpine.start()
